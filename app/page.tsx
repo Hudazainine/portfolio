68c0729540df6311c88877451e2e7e27125594ea
Hudazainine <houdazainine060@gmail.com>
2025-12-05 01:14:14 +0100

    message

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Houda
           </a>
         </div>
       </main>
