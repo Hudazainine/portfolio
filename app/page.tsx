1c246612d3f77164e453ad2a0618a43c8eb730b1
Hudazainine <houdazainine060@gmail.com>
2025-12-06 16:38:22 +0100

    message

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,216 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+    <div>
+      <meta charSet="utf-8" />
+      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+      <title>Index - Personal Bootstrap Template</title>
+      <meta name="description" content="Your description here" />
+      <meta name="keywords" content="Your keywords here" />
+      {/* Favicons */}
+      <link href="assets/img/favicon.png" rel="icon" />
+      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+      {/* Fonts */}
+      <link href="https://fonts.googleapis.com" rel="preconnect" />
+      <link
+        href="https://fonts.gstatic.com"
+        rel="preconnect"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      />
+      {/* Vendor CSS Files */}
+      <link
+        href="assets/vendor/bootstrap/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
+        rel="stylesheet"
+      />
+      <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+      <link
+        href="assets/vendor/swiper/swiper-bundle.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="assets/vendor/glightbox/css/glightbox.min.css"
+        rel="stylesheet"
+      />
+      {/* Main CSS File */}
+      <link href="assets/css/main.css" rel="stylesheet" />
+      {/* =======================================================
+  * Template Name: Personal
+  * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+  * Updated: Mar 05 2025 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== */}
+      <header
+        id="header"
+        className="header d-flex align-items-center fixed-top"
+      >
+        <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+          <a href="index.html" className="logo d-flex align-items-center">
+            {/* Uncomment the line below if you also wish to use an image logo */}
+            {/* <img src="assets/img/logo.webp" alt=""> */}
+            <h1 className="sitename">Personal</h1>
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Houda
-          </a>
+          <nav id="navmenu" className="navmenu">
+            <ul>
+              <li>
+                <a href="index.html" className="active">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="about.html">About</a>
+              </li>
+              <li>
+                <a href="resume.html">Resume</a>
+              </li>
+              <li>
+                <a href="services.html">Services</a>
+              </li>
+              <li>
+                <a href="portfolio.html">Portfolio</a>
+              </li>
+              <li className="dropdown">
+                <a href="#">
+                  <span>Dropdown</span>{" "}
+                  <i className="bi bi-chevron-down toggle-dropdown" />
+                </a>
+                <ul>
+                  <li>
+                    <a href="#">Dropdown 1</a>
+                  </li>
+                  <li className="dropdown">
+                    <a href="#">
+                      <span>Deep Dropdown</span>{" "}
+                      <i className="bi bi-chevron-down toggle-dropdown" />
+                    </a>
+                    <ul>
+                      <li>
+                        <a href="#">Deep Dropdown 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Dropdown 2</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Dropdown 3</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Dropdown 4</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Dropdown 5</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Dropdown 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Dropdown 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Dropdown 4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="contact.html">Contact</a>
+              </li>
+            </ul>
+            <i className="mobile-nav-toggle d-xl-none bi bi-list" />
+          </nav>
         </div>
+      </header>
+      <main className="main">
+        {/* Hero Section */}
+        <section id="hero" className="hero section dark-background">
+          <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in" />
+          <div className="container" data-aos="zoom-out" data-aos-delay={100}>
+            <h2>Emily Jones</h2>
+            <p>
+              I'm{" "}
+              <span
+                className="typed"
+                data-typed-items="Designer, Developer, Freelancer, Photographer"
+              />
+              <span className="typed-cursor typed-cursor--blink" />
+            </p>
+            <div className="social-links">
+              <a href="#">
+                <i className="bi bi-twitter-x" />
+              </a>
+              <a href="#">
+                <i className="bi bi-facebook" />
+              </a>
+              <a href="#">
+                <i className="bi bi-instagram" />
+              </a>
+              <a href="#">
+                <i className="bi bi-linkedin" />
+              </a>
+            </div>
+          </div>
+        </section>
+        {/* /Hero Section */}
       </main>
+      <footer id="footer" className="footer dark-background">
+        <div className="container">
+          <h3 className="sitename">Personal</h3>
+          <p>
+            Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni
+            eligendi fuga maxime saepe commodi placeat.
+          </p>
+          <div className="social-links d-flex justify-content-center">
+            <a href="#">
+              <i className="bi bi-twitter-x" />
+            </a>
+            <a href="#">
+              <i className="bi bi-facebook" />
+            </a>
+            <a href="#">
+              <i className="bi bi-instagram" />
+            </a>
+            <a href="#">
+              <i className="bi bi-skype" />
+            </a>
+            <a href="#">
+              <i className="bi bi-linkedin" />
+            </a>
+          </div>
+          <div className="container">
+            <div className="copyright">
+              <span>Copyright</span>{" "}
+              <strong className="px-1 sitename">Personal</strong>{" "}
+              <span>All Rights Reserved</span>
+            </div>
+            <div className="credits">
+              {/* All the links in the footer should remain intact. */}
+              {/* You can delete the links only if you've purchased the pro version. */}
+              {/* Licensing information: https://bootstrapmade.com/license/ */}
+              {/* Purchase the pro version with working PHP/AJAX contact form: [buy-url] */}
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* Scroll Top */}
+      <a
+        href="#"
+        id="scroll-top"
+        className="scroll-top d-flex align-items-center justify-content-center"
+      >
+        <i className="bi bi-arrow-up-short" />
+      </a>
+      {/* Preloader */}
+      <div id="preloader" />
+      {/* Vendor JS Files */}
+      {/* Main JS File */}
     </div>
   );
 }
