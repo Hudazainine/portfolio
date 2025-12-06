1777bd8b8e09545cfdc709792a1edea8a6506ad3
Hudazainine <houdazainine060@gmail.com>
2025-12-06 20:55:41 +0100

    Fix Typed.js and AOS for production build

--- app/page.tsx
+++ app/page.tsx
@@ -25,5 +25,18 @@ export default function Home() {
       });
     }
   }, []);
-  return <>{/* Ton code JSX ici */}</>;
+  return (
+    <>
+      {
+        <div>
+          {/* Hello world */}
+          <div className="awesome" style={{ border: "1px solid red" }}>
+            <label htmlFor="name">Enter your name: </label>
+            <input type="text" id="name" />
+          </div>
+          <p>Enter your HTML here</p>
+        </div>
+      }
+    </>
+  );
 }
