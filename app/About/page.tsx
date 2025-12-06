d5be15d9f5a81e01613e4b6303c4b862f49caf11
Hudazainine <houdazainine060@gmail.com>
2025-12-07 00:42:45 +0100

    Update layout and packages

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,4 +1,3 @@
-// app/about/About.tsx
 "use client";
 
 export default function About() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import { useEffect } from "react";
-import Head from "next/head";
 import Link from "next/link";
+
 export default function Home() {
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -22,55 +23,7 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Index - Personal Bootstrap Template</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-
-        {/* Favicons */}
-        <link href="assets/img/favicon.png" rel="icon" />
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
-
-        {/* Fonts */}
-        <link href="https://fonts.googleapis.com" rel="preconnect" />
-        <link
-          href="https://fonts.gstatic.com"
-          rel="preconnect"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Vendor CSS Files */}
-        <link
-          href="assets/vendor/bootstrap/css/bootstrap.min.css"
-          rel="stylesheet"
-        />
-        <link
-          href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
-          rel="stylesheet"
-        />
-        <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
-        <link
-          href="assets/vendor/swiper/swiper-bundle.min.css"
-          rel="stylesheet"
-        />
-        <link
-          href="assets/vendor/glightbox/css/glightbox.min.css"
-          rel="stylesheet"
-        />
-
-        {/* Main CSS File */}
-        <link href="assets/css/main.css" rel="stylesheet" />
-      </Head>
-      <header
-        id="header"
-        className="header d-flex align-items-center fixed-top"
-      >
+      <header className="header d-flex align-items-center fixed-top">
         <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
           <a href="#" className="logo d-flex align-items-center">
             <h1 className="sitename">Houda.dev</h1>
@@ -86,7 +39,6 @@ export default function Home() {
               <li>
                 <Link href="/about">About</Link>
               </li>
-
               <li>
                 <a href="#">Resume</a>
               </li>
@@ -103,8 +55,12 @@ export default function Home() {
       </header>
 
       <main className="main">
-        <section id="hero" className="hero section ">
-          <img src="/public/assets/houda.png" alt="" data-aos="fade-in" />
+        <section id="hero" className="hero section">
+          <img
+            src="/assets/img/hero-bg.jpg"
+            alt="Hero Background"
+            data-aos="fade-in"
+          />
 
           <div className="container" data-aos="zoom-out" data-aos-delay="100">
             <h2>Houda Zaïnine</h2>
@@ -132,43 +88,6 @@ export default function Home() {
           </div>
         </section>
       </main>
-
-      <footer id="footer" className="footer ">
-        <div className="container">
-          <h3 className="sitename">Personal</h3>
-          <p>Et aut eum quis fuga eos sunt ipsa nihil.</p>
-
-          <div className="social-links d-flex justify-content-center">
-            <a href="#">
-              <i className="bi bi-twitter-x"></i>
-            </a>
-            <a href="#">
-              <i className="bi bi-facebook"></i>
-            </a>
-            <a href="#">
-              <i className="bi bi-instagram"></i>
-            </a>
-            <a href="#">
-              <i className="bi bi-skype"></i>
-            </a>
-            <a href="#">
-              <i className="bi bi-linkedin"></i>
-            </a>
-          </div>
-        </div>
-      </footer>
-
-      {/* Scroll Top */}
-      <a
-        href="#"
-        id="scroll-top"
-        className="scroll-top d-flex align-items-center justify-content-center"
-      >
-        <i className="bi bi-arrow-up-short"></i>
-      </a>
-
-      {/* Preloader */}
-      <div id="preloader"></div>
     </>
   );
 }
