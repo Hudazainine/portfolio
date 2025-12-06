1c25d648a33cc95bd77ab0d3cf7491fbf5b45fb8
Hudazainine <houdazainine060@gmail.com>
2025-12-06 16:46:13 +0100

    message

--- app/page.tsx
+++ app/page.tsx
@@ -1,85 +1,55 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    // plugins JS exécutés côté client
+    if (typeof window !== "undefined") {
+      require("aos/dist/aos.css");
+      require("aos").init();
+      require("typed.js");
+    }
+  }, []);
+
   return (
-    <div>
-      <meta charSet="utf-8" />
-      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <title>Index - Personal Bootstrap Template</title>
-      <meta name="description" content="Your description here" />
-      <meta name="keywords" content="Your keywords here" />
-      {/* Favicons */}
-      <link href="assets/img/favicon.png" rel="icon" />
-      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
-      {/* Fonts */}
-      <link href="https://fonts.googleapis.com" rel="preconnect" />
-      <link
-        href="https://fonts.gstatic.com"
-        rel="preconnect"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet"
-      />
-      {/* Vendor CSS Files */}
-      <link
-        href="assets/vendor/bootstrap/css/bootstrap.min.css"
-        rel="stylesheet"
-      />
-      <link
-        href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
-        rel="stylesheet"
-      />
-      <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
-      <link
-        href="assets/vendor/swiper/swiper-bundle.min.css"
-        rel="stylesheet"
-      />
-      <link
-        href="assets/vendor/glightbox/css/glightbox.min.css"
-        rel="stylesheet"
-      />
-      {/* Main CSS File */}
-      <link href="assets/css/main.css" rel="stylesheet" />
-      {/* =======================================================
-  * Template Name: Personal
-  * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-  * Updated: Mar 05 2025 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== */}
+    <>
+      {/* Header */}
       <header
         id="header"
         className="header d-flex align-items-center fixed-top"
       >
         <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-          <a href="index.html" className="logo d-flex align-items-center">
-            {/* Uncomment the line below if you also wish to use an image logo */}
-            {/* <img src="assets/img/logo.webp" alt=""> */}
+          <Link href="/" className="logo d-flex align-items-center">
             <h1 className="sitename">Personal</h1>
-          </a>
+          </Link>
+
           <nav id="navmenu" className="navmenu">
             <ul>
               <li>
-                <a href="index.html" className="active">
+                <Link href="/" className="active">
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="about.html">About</a>
+                <Link href="/about">About</Link>
               </li>
               <li>
-                <a href="resume.html">Resume</a>
+                <Link href="/resume">Resume</Link>
               </li>
               <li>
-                <a href="services.html">Services</a>
+                <Link href="/services">Services</Link>
               </li>
               <li>
-                <a href="portfolio.html">Portfolio</a>
+                <Link href="/portfolio">Portfolio</Link>
               </li>
+
               <li className="dropdown">
                 <a href="#">
                   <span>Dropdown</span>{" "}
-                  <i className="bi bi-chevron-down toggle-dropdown" />
+                  <i className="bi bi-chevron-down toggle-dropdown"></i>
                 </a>
                 <ul>
                   <li>
@@ -88,7 +58,7 @@ export default function Home() {
                   <li className="dropdown">
                     <a href="#">
                       <span>Deep Dropdown</span>{" "}
-                      <i className="bi bi-chevron-down toggle-dropdown" />
+                      <i className="bi bi-chevron-down toggle-dropdown"></i>
                     </a>
                     <ul>
                       <li>
@@ -119,46 +89,57 @@ export default function Home() {
                   </li>
                 </ul>
               </li>
+
               <li>
-                <a href="contact.html">Contact</a>
+                <Link href="/contact">Contact</Link>
               </li>
             </ul>
-            <i className="mobile-nav-toggle d-xl-none bi bi-list" />
+            <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
           </nav>
         </div>
       </header>
+
+      {/* Hero Section */}
       <main className="main">
-        {/* Hero Section */}
         <section id="hero" className="hero section dark-background">
-          <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in" />
-          <div className="container" data-aos="zoom-out" data-aos-delay={100}>
+          <Image
+            src="/assets/img/hero-bg.jpg"
+            alt=""
+            fill
+            className="hero-bg"
+            data-aos="fade-in"
+          />
+
+          <div className="container" data-aos="zoom-out" data-aos-delay="100">
             <h2>Emily Jones</h2>
             <p>
               I'm{" "}
               <span
                 className="typed"
                 data-typed-items="Designer, Developer, Freelancer, Photographer"
-              />
-              <span className="typed-cursor typed-cursor--blink" />
+              ></span>
+              <span className="typed-cursor typed-cursor--blink"></span>
             </p>
+
             <div className="social-links">
               <a href="#">
-                <i className="bi bi-twitter-x" />
+                <i className="bi bi-twitter-x"></i>
               </a>
               <a href="#">
-                <i className="bi bi-facebook" />
+                <i className="bi bi-facebook"></i>
               </a>
               <a href="#">
-                <i className="bi bi-instagram" />
+                <i className="bi bi-instagram"></i>
               </a>
               <a href="#">
-                <i className="bi bi-linkedin" />
+                <i className="bi bi-linkedin"></i>
               </a>
             </div>
           </div>
         </section>
-        {/* /Hero Section */}
       </main>
+
+      {/* Footer */}
       <footer id="footer" className="footer dark-background">
         <div className="container">
           <h3 className="sitename">Personal</h3>
@@ -166,23 +147,25 @@ export default function Home() {
             Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni
             eligendi fuga maxime saepe commodi placeat.
           </p>
+
           <div className="social-links d-flex justify-content-center">
-            <a href="#">
-              <i className="bi bi-twitter-x" />
+            <a href="">
+              <i className="bi bi-twitter-x"></i>
             </a>
-            <a href="#">
-              <i className="bi bi-facebook" />
+            <a href="">
+              <i className="bi bi-facebook"></i>
             </a>
-            <a href="#">
-              <i className="bi bi-instagram" />
+            <a href="">
+              <i className="bi bi-instagram"></i>
             </a>
-            <a href="#">
-              <i className="bi bi-skype" />
+            <a href="">
+              <i className="bi bi-skype"></i>
             </a>
-            <a href="#">
-              <i className="bi bi-linkedin" />
+            <a href="">
+              <i className="bi bi-linkedin"></i>
             </a>
           </div>
+
           <div className="container">
             <div className="copyright">
               <span>Copyright</span>{" "}
@@ -190,27 +173,21 @@ export default function Home() {
               <span>All Rights Reserved</span>
             </div>
             <div className="credits">
-              {/* All the links in the footer should remain intact. */}
-              {/* You can delete the links only if you've purchased the pro version. */}
-              {/* Licensing information: https://bootstrapmade.com/license/ */}
-              {/* Purchase the pro version with working PHP/AJAX contact form: [buy-url] */}
               Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
             </div>
           </div>
         </div>
       </footer>
-      {/* Scroll Top */}
+
       <a
         href="#"
         id="scroll-top"
         className="scroll-top d-flex align-items-center justify-content-center"
       >
-        <i className="bi bi-arrow-up-short" />
+        <i className="bi bi-arrow-up-short"></i>
       </a>
-      {/* Preloader */}
-      <div id="preloader" />
-      {/* Vendor JS Files */}
-      {/* Main JS File */}
-    </div>
+
+      <div id="preloader"></div>
+    </>
   );
 }
