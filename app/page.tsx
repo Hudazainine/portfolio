81c26c06ee6b65a96e2f228a47ffbb40754245aa
Hudazainine <houdazainine060@gmail.com>
2025-12-06 21:55:17 +0100

    Re-save state of main branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,135 @@
 "use client";
 
 import { useEffect } from "react";
-import "aos/dist/aos.css";
 import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      AOS.init();
+    AOS.init();
 
-      import("typed.js").then(({ default: Typed }) => {
-        const el = document.querySelector(".typed");
-        if (el) {
-          new Typed(el, {
-            strings: ["Designer", "Developer", "Freelancer", "Photographer"],
-            typeSpeed: 50,
-            backSpeed: 25,
-            loop: true,
-          });
-        }
-      });
-    }
+    import("typed.js").then(({ default: Typed }) => {
+      const element = document.querySelector(".typed");
+      if (element) {
+        new Typed(element, {
+          strings: ["Designer", "Developer", "Freelancer", "Photographer"],
+          typeSpeed: 50,
+          backSpeed: 25,
+          loop: true,
+        });
+      }
+    });
   }, []);
 
   return (
-    <div className="awesome" style={{ border: "1px solid red" }}>
-      <label htmlFor="name">Enter your name: </label>
-      <input type="text" id="name" />
-    </div>
+    <>
+      <header
+        id="header"
+        className="header d-flex align-items-center fixed-top"
+      >
+        <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+          <a href="#" className="logo d-flex align-items-center">
+            <h1 className="sitename">Personal</h1>
+          </a>
+
+          <nav id="navmenu" className="navmenu">
+            <ul>
+              <li>
+                <a href="#" className="active">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Resume</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+            </ul>
+            <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+        </div>
+      </header>
+
+      <main className="main">
+        <section id="hero" className="hero section dark-background">
+          <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in" />
+
+          <div className="container" data-aos="zoom-out" data-aos-delay="100">
+            <h2>Emily Jones</h2>
+            <p>
+              I'm{" "}
+              <span
+                className="typed"
+                data-typed-items="Designer, Developer, Freelancer, Photographer"
+              ></span>
+            </p>
+            <div className="social-links">
+              <a href="#">
+                <i className="bi bi-twitter-x"></i>
+              </a>
+              <a href="#">
+                <i className="bi bi-facebook"></i>
+              </a>
+              <a href="#">
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="bi bi-linkedin"></i>
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer id="footer" className="footer dark-background">
+        <div className="container">
+          <h3 className="sitename">Personal</h3>
+          <p>Et aut eum quis fuga eos sunt ipsa nihil.</p>
+
+          <div className="social-links d-flex justify-content-center">
+            <a href="#">
+              <i className="bi bi-twitter-x"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-facebook"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-skype"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+
+      {/* Scroll Top */}
+      <a
+        href="#"
+        id="scroll-top"
+        className="scroll-top d-flex align-items-center justify-content-center"
+      >
+        <i className="bi bi-arrow-up-short"></i>
+      </a>
+
+      {/* Preloader */}
+      <div id="preloader"></div>
+
+      {/* Vendor JS */}
+      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/aos/aos.js"></script>
+      <script src="assets/vendor/typed.js/typed.umd.js"></script>
+      <script src="assets/js/main.js"></script>
+    </>
   );
 }
